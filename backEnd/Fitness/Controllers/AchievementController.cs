4f2f62819b294d17bea483e4340740b356d7e4cf
allagod <139415044+allagod@users.noreply.github.com>
2024-08-21 17:39:07 +0800

    Add files via upload

---
+++ backEnd/Fitness/Controllers/AchievementController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using Fitness.DAL;
+using Fitness.BLL;
+using Fitness.Models;
+
+namespace Fitness.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class AchievementController : ControllerBase
+    {
+
+        [HttpGet]
+        public string GetAchievement(string token)
+        {
+            TokenValidationResult tokenRes = _jwtHelper.ValidateToken(token);
+            int userId = tokenRes.userID;
+            return UserAchievementBll.GetUserAchievement(userId);
+        }
+
+        [HttpGet]
+        public string GetAchievementRanking(string token, int achievementId)
+        {
+            TokenValidationResult tokenRes = _jwtHelper.ValidateToken(token);
+            int userId = tokenRes.userID;
+            return UserAchievementBll.GetAchievementRank(userId, achievementId);
+        }
+
+        [HttpPost]
+        public string UpdateFitnessPlanAchievement(string token)
+        {
+            TokenValidationResult tokenRes = _jwtHelper.ValidateToken(token);
+            int userId = tokenRes.userID;
+            return UserAchievementBll.UpdateFitnessPlanAchievement(userId);
+        }
+    }
+}
---
+++ backEnd/Fitness/Controllers/FitnessPlanController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using Fitness.DAL;
+using Fitness.BLL;
+using Fitness.Models;
+
+namespace Fitness.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class FitnessPlanController : ControllerBase
+    {
+        [HttpPost]
+        public string PostFitness(string token, double height, double weight, double BMI, double bodyFatRate)
+        {
+            TokenValidationResult tokenRes = _jwtHelper.ValidateToken(token);
+            int userId = tokenRes.userID;
+            return FitnessBll.Update(userId, height, weight, BMI, bodyFatRate);
+        }
+
+        [HttpPost]
+        public string PostPhysicalTest(string token, int pushups, int squats, int situps, int pullups, int longDistance)
+        {
+            TokenValidationResult tokenRes = _jwtHelper.ValidateToken(token);
+            int userId = tokenRes.userID;
+            return PhysicalTestBll.Update(userId, pushups, squats, situps, pullups, longDistance);
+        }
+
+        [HttpGet]
+        public string GetFitness(string token)
+        {
+            TokenValidationResult tokenRes = _jwtHelper.ValidateToken(token);
+            int userId = tokenRes.userID;
+            return FitnessBll.Get(userId);
+        }
+
+        [HttpGet]
+        public string GetPhysicalTest(string token)
+        {
+            TokenValidationResult tokenRes = _jwtHelper.ValidateToken(token);
+            int userId = tokenRes.userID;
+            return PhysicalTestBll.Get(userId);
+        }
+
+        [HttpPost]
+        public string SetGoal(string token, string goal, int duration)
+        {
+            TokenValidationResult tokenRes = _jwtHelper.ValidateToken(token);
+            int userId = tokenRes.userID;
+            return UserFitnessPlanGoalBll.Set(userId, goal, duration);
+        }
+
+        [HttpGet]
+        public string GetGoal(string token)
+        {
+            TokenValidationResult tokenRes = _jwtHelper.ValidateToken(token);
+            int userId = tokenRes.userID;
+            return UserFitnessPlanGoalBll.Get(userId);
+        }
+
+        [HttpGet]
+        public string GetPlan(string token)
+        {
+            TokenValidationResult tokenRes = _jwtHelper.ValidateToken(token);
+            int userId = tokenRes.userID;
+            return WorkoutBll.GetPlan(userId);
+        }
+    }
+}
