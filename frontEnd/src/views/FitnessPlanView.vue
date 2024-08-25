4d5c580bf977a2584bda8bcc27796f496737a557
allagod <139415044+allagod@users.noreply.github.com>
2024-08-26 01:50:05 +0800

    Add files via upload

---
+++ frontEnd/src/views/FitnessPlanView.vue
@@ -0,0 +1,240 @@
+<script setup lang="ts">
+
+import { reactive, ref } from 'vue'
+import type { ComponentSize, FormInstance, FormRules } from 'element-plus'
+import type { Action } from 'element-plus'
+import NavigationBar from "@/components/NavigationBar.vue";
+import TimeThread from "@/components/TimeThread.vue";
+import axios from "axios";
+const dialogFormVisible = ref(false)
+const formLabelWidth = '110px'
+interface RuleForm {
+  height: number,
+  weight: number,
+  BMI: number,
+  bodyFatRate:  number,
+  pushups:  number,
+  squats: number,
+  situps:  number,
+  pullup:  number,
+  longDistance: number,
+  goal: string,
+}
+const ruleFormRef = ref<FormInstance>()
+const form = reactive<RuleForm>({
+  height: null,
+  weight: null,
+  BMI: null,
+  bodyFatRate:null,
+  pushups: null,
+  squats: null,
+  situps: null,
+  pullup: null,
+  longDistance:null,
+  goal:'',
+})
+const rules = reactive<FormRules<RuleForm>>({
+  height: [
+    { required: true, message: '请输入身高', trigger: 'blur' },
+    { type: 'number', message: '请输入数字', trigger: 'blur' },
+  ],
+  weight: [
+    { required: true, message: '请输入体重', trigger: 'blur' },
+    { type: 'number', message: '请输入数字', trigger: 'blur' },
+  ],
+  BMI: [
+    { required: true, message: '请输入BMI', trigger: 'blur' },
+    { type: 'number', message: '请输入数字', trigger: 'blur' },
+  ],
+  bodyFatRate: [
+    { required: true, message: '请输入体脂率', trigger: 'blur' },
+    { type: 'number', message: '请输入数字', trigger: 'blur' },
+  ],
+  pushups: [
+    { required: true, message: '请输入俯卧撑个数', trigger: 'blur' },
+    { type: 'integer', message: '请输入整数', trigger: 'blur' },
+  ],
+  squats: [
+    { required: true, message: '请输入深蹲个数', trigger: 'blur' },
+    { type: 'integer', message: '请输入整数', trigger: 'blur' },
+  ],
+  situps: [
+    { required: true, message: '请输入仰卧起坐个数', trigger: 'blur' },
+    { type: 'integer', message: '请输入整数', trigger: 'blur' },
+  ],
+  pullup: [
+    { required: true, message: '请输入引体向上个数', trigger: 'blur' },
+    { type: 'integer', message: '请输入整数', trigger: 'blur' },
+  ],
+  longDistance: [
+    { required: true, message: '请输入一千米时间', trigger: 'blur' },
+    { type: 'integer', message: '请输入整数', trigger: 'blur' },
+  ],
+  goal: [
+    { required: true, message: '请选择健身目标', trigger: 'blur' },
+  ],
+
+})
+const loading = ref(true);
+const submitForm = async (formEl: FormInstance | undefined) => {
+  if (!formEl) return
+  await formEl.validate((valid, fields) => {
+    if (valid) {
+      console.log('submit!')
+      dialogFormVisible.value = !dialogFormVisible.value;
+      submit()
+    } else {
+      console.log('error submit!', fields)
+    }
+  })
+
+
+}
+function submit() {
+  loading.value = true;
+  console.log(form.pullup)
+  axios.get('http://localhost:5000/api/FitnessPlan/PostFitness',  {
+    params: {
+      token: localStorage.getItem('token'),
+      height: form.height,
+      weight: form.weight,
+      BMI: form.BMI,
+      bodyFatRate: form.bodyFatRate
+    }}).then(response => {
+    console.log(response.data.message);
+  });
+  axios.get('http://localhost:5000/api/FitnessPlan/PostPhysicalTest',  {
+    params: {
+      token: localStorage.getItem('token'),
+      pushups: form.pushups,
+      squats: form.squats,
+      situps: form.situps,
+      pullup: form.pullup,
+      longDistance: form.longDistance
+    }}).then(response => {
+    console.log(response.data.message);
+  });
+  axios.get('http://localhost:5000/api/FitnessPlan/SetGoal',  {
+    params: {
+      token: localStorage.getItem('token'),
+      goal: form.goal,
+      duration:4
+    }}).then(response => {
+    console.log(response.data.message);
+  });
+}
+const activeName = ref(1);
+</script>
+
+<template>
+  <NavigationBar/>
+  <div>
+    <el-button plain @click="dialogFormVisible = true" class="bot">
+      填写你的体测表
+    </el-button>
+  </div>
+  <el-dialog v-model="dialogFormVisible" title="体测信息" width="400">
+    <el-form :model="form" :rules="rules"  ref="ruleFormRef">
+      <el-form-item label="身高(cm)" :label-width="formLabelWidth" prop="height">
+        <el-input-number v-model.number="form.height" autocomplete="off"  :precision="2" :controls="false"></el-input-number>
+      </el-form-item>
+      <el-form-item label="体重(kg)" :label-width="formLabelWidth" prop="weight">
+        <el-input-number v-model.number="form.weight" autocomplete="off"  :precision="2" :controls="false"></el-input-number>
+      </el-form-item>
+      <el-form-item label="BMI" :label-width="formLabelWidth" prop="BMI">
+        <el-input-number v-model.number="form.BMI" autocomplete="off"  :precision="1" :controls="false"></el-input-number>
+      </el-form-item>
+      <el-form-item label="体脂率" :label-width="formLabelWidth" prop="bodyFatRate">
+        <el-input-number v-model.number="form.bodyFatRate" autocomplete="off"  :precision="1" :controls="false"></el-input-number>
+      </el-form-item>
+      <el-form-item label="俯卧撑个数" :label-width="formLabelWidth" prop="pushups">
+        <el-input-number v-model.number="form.pushups" autocomplete="off"  :controls="false"></el-input-number>
+      </el-form-item>
+      <el-form-item label="深蹲个数" :label-width="formLabelWidth" prop="squats">
+        <el-input-number v-model.number="form.squats" autocomplete="off"  :controls="false"></el-input-number>
+      </el-form-item>
+      <el-form-item label="仰卧起坐个数" :label-width="formLabelWidth" prop="situps">
+        <el-input-number v-model.number="form.situps" autocomplete="off"  :controls="false"></el-input-number>
+      </el-form-item>
+      <el-form-item label="引体向上个数" :label-width="formLabelWidth" prop="pullup">
+        <el-input-number v-model.number="form.pullup" autocomplete="off"  :controls="false"></el-input-number>
+      </el-form-item>
+      <el-form-item label="一千米时间(s)" :label-width="formLabelWidth" prop="longDistance">
+        <el-input-number v-model.number="form.longDistance" autocomplete="off"  :controls="false"></el-input-number>
+      </el-form-item>
+      <el-form-item label="健身目标" :label-width="formLabelWidth" prop="goal">
+        <el-select v-model="form.goal" placeholder="Please select a target">
+          <el-option label="减脂" value="loseWeight" />
+          <el-option label="增肌" value="buildMuscle" />
+          <el-option label="塑型" value="bodySculpting" />
+        </el-select>
+      </el-form-item>
+
+
+
+
+    </el-form>
+    <template #footer>
+      <div class="dialog-footer">
+        <el-button @click="dialogFormVisible = false">Cancel</el-button>
+        <el-button  type="primary" @click="submitForm(ruleFormRef)">
+          Create
+        </el-button>
+      </div>
+    </template>
+  </el-dialog>
+
+
+  <TimeThread></TimeThread>
+
+
+
+
+
+
+</template>
+
+<style>
+.bot{
+  position:absolute;
+  left: 320px;
+  top: 100px;
+  width: 150px;
+  height: 100px;
+}
+
+.title {
+  font-size: 18px;
+  font-weight: bold;
+  color: #303133;
+  padding-bottom: 17px;
+  padding-top: 17px;
+}
+/* 自定义 el-collapse-item 的样式 */
+.custom-collapse-item .el-collapse-item__header {
+  font-size: 18px;
+  font-weight: bold;
+  color: #1f2d3d;
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+  padding: 10px;
+  border-bottom: 1px solid #ebeef5;
+  transition: all 0.3s ease;
+  background-color: transparent;
+}
+
+/* 为标题添加图标 */
+.custom-collapse-item .el-collapse-item__header::before {
+  content: '📅';
+  margin-right: 10px;
+}
+
+/* 鼠标悬停时标题的样式 */
+.custom-collapse-item .el-collapse-item__header:hover {
+  background-color: #e6f7ff;
+  color: #409eff;
+}
+
+</style>
+
