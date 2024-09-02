7e21bcc796145f33d20c51e9ecba18296f59e9d8
zeizeiwaii <148342667+zeizeiwaii@users.noreply.github.com>
2024-09-02 17:43:30 +0800

    补充了排行榜

--- frontEnd/src/router/index.js
+++ frontEnd/src/router/index.js
@@ -184,6 +184,12 @@ const router = createRouter({
       component: AdminView,
       props: true,
     },
+    // 排行榜界面
+    {
+      path: '/ranking-list',
+      name: 'RankingList',
+      component: RankingList,
+    },
   ],
 });
 
