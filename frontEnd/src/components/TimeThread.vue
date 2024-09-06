0bf59fb4dfe9c1ed9db9c81d627e3f29c90aad66
mutition <2397997105@qq.com>
2024-09-06 16:59:39 +0800

    构建健身计划完成日历

--- frontEnd/src/components/TimeThread.vue
+++ frontEnd/src/components/TimeThread.vue
@@ -3,6 +3,7 @@
     生成健身计划
   </el-button>
 
+
   <el-collapse class="list" v-model="activeName">
     <el-collapse-item
       v-for="(week, index) in weeks.plan"
@@ -42,27 +43,47 @@
                   </el-table-column>
                 </el-table>
               </div>
-              <el-button @click="finish(index*7+index2)" class="over" type="primary" :disabled="buttonDisabled[index*7+index2]"> {{ buttonText[index*7+index2] }}</el-button>
+              <el-button @click="finish(index,index2)" class="over" type="primary" :disabled="isButtonDisabled(index,index2) || buttonDisabled[index*7+index2]"> {{(isButtonDisabled(index,index2)|| buttonDisabled[index*7+index2])?'今日计划已完成':'完成计划'}}</el-button>
             </el-popover>
           </el-container>
         </el-timeline-item>
       </el-timeline>
     </el-collapse-item>
   </el-collapse>
+  <el-popover placement="bottom" :width="400" trigger="click" >
+    <template #reference>
+      <el-button style="margin-right: 16px" class="map" type="primary">查看我的健身进度</el-button>
+    </template>
+    <el-calendar :range="[startDate, endDate]" >
+      <!-- 自定义 date-cell 插槽 -->
+      <template #date-cell="{ data }">
+        <div :class="getCellStyle(data.day)"></div>
+      </template>
+    </el-calendar>
+    <p>本月计划已完成<span class="bold-text">{{ count }}</span>天</p>
+  </el-popover>
 
 
-  <el-calendar
-      :range="[startDate, endDate]"
-      :date-cell="customDateCell"
-  ></el-calendar>
 </template>
 
 <style scoped>
+.bold-text {
+  font-size: 30px;
+  font-weight: bold; /* 字体加粗 */
+  color:red;
+}
 .button{
   position: absolute;
   left: 32%;
   top: 12px;
   width: 100px;
+  height: 40px;
+}
+.map{
+  position: absolute;
+  left: 70%;
+  top: 12px;
+  width: 130px;
   height: 40px;
 }
 .list{
@@ -102,11 +123,22 @@
 >>>.el-card:hover{
   box-shadow:5px 10px 16px #0ff !important;
 }
+
 >>>.el-card:active{
   box-shadow:5px 10px 16px #ff5858 !important;
 }
 >>>.el-calendar__header {
   display: none;
+}
+>>>.el-calendar-table thead th {
+  display: none ;
+}
+
+>>> .el-calendar-table .el-calendar-day:hover {
+  background-color: inherit;
+}
+p {
+  font-size: 20px;
 }
 .card {
   max-width: 300px;
@@ -140,7 +172,21 @@
 .gif:hover {
   content: url(../assets/images/strength.png); /* 鼠标悬停时显示的图片路径 */
 }
+>>>.el-calendar-table .el-calendar-day{
+  width: 60px;
+  height: 40px;
+}
+>>>.el-calendar {
+  width: 350px;
+  height: 200px;
+  padding-bottom: 0;
+}
 
+.calendar-day {
+  height: 30px;
+  width: 30px;
+  background-color: blue;
+}
 .dialog-video{
   height:65%;
 }
@@ -151,40 +197,11 @@
 </style>
 
 <script lang="ts" setup>
-import { ref, onMounted } from 'vue';
-import {ElMessageBox, ElNotification} from 'element-plus';
+import { ref, onMounted ,onBeforeMount,computed} from 'vue';
+import {ElCalendar,ElMessageBox, ElNotification} from 'element-plus';
 import axios from 'axios';
 import dayjs from 'dayjs';
 
-// 设置特定的开始日期，比如 2024年9月1日
-const startDate = ref(dayjs('2024-09-01').toDate());
-// 设置结束日期为28天后的日期
-const endDate = ref(dayjs(startDate.value).add(27, 'day').toDate());
-// 定义需要背景变蓝色的日期数组
-const blueDays = ['2024-09-03', '2024-09-10', '2024-09-15'];
-const customDateCell = ({ date }) => {
-  const dateString = dayjs(date).format('YYYY-MM-DD'); // 将日期格式化为字符串
-  const isBlueDay = blueDays.includes(dateString); // 判断是否是蓝色背景的日期
-
-  return h(
-      'div',
-      {
-        style: {
-          backgroundColor: isBlueDay ? 'blue' : '', // 设置蓝色背景
-          color: isBlueDay ? 'white' : 'black', // 确保蓝色背景下文字可读
-          padding: '10px',
-          borderRadius: '5px',
-        },
-      },
-      date.getDate() // 显示日期
-  );
-};
-
-const weeks = ref([]);
-const titles = ["第一周", "第二周", "第三周", "第四周"];
-const loading = ref(true);
-const activeName = ref([0]);
-const isDisable=ref(true);
 const buttonDisabled=ref([false, false, false, false, false, false, false,
   false, false, false, false, false, false, false,
   false, false, false, false, false, false, false,
@@ -193,13 +210,74 @@ const buttonText=ref(['完成计划','完成计划','完成计划','完成计划
   '完成计划','完成计划','完成计划','完成计划','完成计划','完成计划','完成计划',
   '完成计划','完成计划','完成计划','完成计划','完成计划','完成计划','完成计划',
   '完成计划','完成计划','完成计划','完成计划','完成计划','完成计划','完成计划',])
-function finish(index){
-  buttonDisabled.value[index]=true;
-  buttonText.value[index]='今日计划已完成';
+const DoneDates=ref(['2024-09-01','2024-09-02','2024-09-03','2024-09-04','2024-09-05','2024-09-06','2024-09-07',
+  '2024-09-08','2024-09-09','2024-09-10','2024-09-11','2024-09-12','2024-09-13','2024-09-14',
+  '2024-09-15','2024-09-16','2024-09-17','2024-09-18','2024-09-19','2024-09-20','2024-09-21',
+  '2024-09-22','2024-09-23','2024-09-24','2024-09-25','2024-09-26','2024-09-27','2024-09-28',])
+// 设置特定的开始日期，比如 2024年9月1日
+const startDate = ref(dayjs('2024-09-01').toDate());
+// 设置结束日期为28天后的日期
+const endDate = ref(dayjs(startDate.value).add(27, 'day').toDate());
+const Get = ref(false)
+// 定义需要背景变蓝色的日期数组
+/// 定义需要背景变蓝的日期数组
+const count = ref(0)
+
+function getCellStyle(day) {
+  if (Get.value==false){
+    Get.value=true
+    axios.get('http://localhost:8080/api/FitnessPlan/GetPlan', {
+      params: {
+        token: localStorage.getItem('token')
+      }
+    }).then(response => {
+      weeks.value = response.data;
+      if(response.data.message == "fail"){
+        return '';
+      }
+      else{
+        for(let i=0;i<4;i++){
+        for(let j=0;j<7;j++){
+          if(weeks.value.plan[i][j].isCompleted=="true"){
+            count.value=count.value+1
+            DoneDates.value[i*7+j]='1';
+          }
+        }
+      }
+        console.log(DoneDates.value)}
+
+    });
+  }
+  if(DoneDates.value.includes(day)){
+    return '';}
+  else{
+    return 'calendar-day';
+  }
+}
+// 判断是否是需要变蓝的日期
+
+
+// 自定义日期单元格
+const isSpecialDate = (date: Date) => {
+  return date.getDate() === 15; // 假设每月的 15 日为特殊日期
+};
+
+
+const weeks = ref([]);
+const titles = ["第一周", "第二周", "第三周", "第四周"];
+const loading = ref(true);
+const activeName = ref([0]);
+const isDisable=ref(true);
+function finish(index,index2){
+  buttonDisabled.value[index*7+index2]=true;
+  buttonText.value[index*7+index2]='今日计划已完成';
+  count.value=count.value+1
+  DoneDates.value[index*7+index2]='1';
+  isButtonDisabled(index,index2);
   axios.get('http://localhost:8080/api/Achievement/UpdateFitnessPlanAchievement', {
     params: {
       token: localStorage.getItem('token'),
-      workoutIndex:index
+      workoutIndex:index*7+index2
     }
   })
 }
@@ -212,24 +290,11 @@ function loadWeeksData() {
   }).then(response => {
     weeks.value = response.data; // 假设数据结构中 weeks 在顶层
     if (response.data.message !== "fail") {
-      console.log(weeks.value.plan[1][1].isCompleted);
       ElNotification({
       message: "健身计划成功生成！",
       type: 'success',
       duration: 2000
     });
-      for(let i=0;i<4;i++){
-        for(let j=0;j<7;j++){
-          if(weeks.value.plan[i][j].isCompleted=="true"){
-            buttonDisabled[i*7+j]=true;
-            buttonText[i*7+j]='今日计划已完成';
-          }
-          else{
-            buttonDisabled[i*7+j]=false;
-            buttonText[i*7+j]='完成计划';
-          }
-        }
-      }
     }
     else{
       ElNotification({
@@ -237,6 +302,7 @@ function loadWeeksData() {
         type: 'error',
         duration: 2000
       });
+      return
     }
     //localStorage.setItem('savedFitnessPlan', JSON.stringify(weeks.value)); // 保存数据到本地存储
     loading.value = false;
@@ -265,8 +331,32 @@ const open = (row) => {
     }
   );
 }
-
-onMounted(() => {
-  loadSavedPlan(); // 组件挂载时加载保存的数据或生成新的计划
+function isButtonDisabled(index,index2) {
+  // 这里可以根据逻辑条件返回 true 或 false
+  // 比如根据某个条件禁用按钮
+  if(weeks.value.plan[index][index2].isCompleted=="true"){
+    return true;
+  }
+  else{
+    return false;
+  }
+}
+onBeforeMount(() => {
+  axios.get('http://localhost:8080/api/FitnessPlan/GetPlan', {
+  params: {
+    token: localStorage.getItem('token')
+  }
+}).then(response => {
+  weeks.value = response.data;
+  for(let i=0;i<4;i++){
+    for(let j=0;j<7;j++){
+      if(weeks.value.plan[i][j].isCompleted=="true"){
+        console.log(111111)
+        DoneDates[i*7+j]='1';
+      }
+    }
+  }
 });
+
+  });
 </script>
